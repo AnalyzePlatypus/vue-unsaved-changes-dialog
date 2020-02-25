ec650cd7e2824a7aeaee88cd6cb9360e57461b28
Michoel Samuels <k2co3@icloud.com>
2020-02-25 11:27:42 +0200

    Fix netlify deploy?

--- demo-site/src/App.vue
+++ demo-site/src/App.vue
@@ -34,7 +34,6 @@ body {
 
 #app {
   min-height: 100vh;
-  // background: lightcoral;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
